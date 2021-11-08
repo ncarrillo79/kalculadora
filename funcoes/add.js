713fbc863d92152feccd4ca5946b9f15926220cb
ncarrillo79 <ncarrillo79@hotmail.com>
2021-11-08 12:07:22 -0300

    refatorações variadas

---
+++ funcoes/add.js
@@ -0,0 +1,20 @@
+function add(a,b){
+
+    if (typeof a==='number' && typeof b==='number'){
+        return res2
+    }else{
+        
+        return false
+    }
+  
+}
+
+console.log(add(a,b))
+
+
+exports.add = add;
+
+
+
+
+console.log(exports)
