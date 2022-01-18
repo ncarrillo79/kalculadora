2d54c5fb4b0f4a16249ffa52566d7b232755d77e
ncarrillo79 <ncarrillo79@hotmail.com>
2022-01-18 12:41:50 -0300

    refatorações com boas práticas

--- src/div.js
+++ src/div.js
@@ -1,20 +1,12 @@
-
-function divide(a,b){
-
-    if (typeof a==='number' && typeof b==='number')
-        if(b!=0){
-        return a/b
-    }else{
-        
-        return false
+function divide(a, b) {
+  if (typeof a === "number" && typeof b === "number")
+    if (b != 0) {
+      return a / b;
+    } else {
+      return false;
     }
-  
 }
-
-//console.log(divide(2,-4))
 
 module.exports.divide = divide;
 
 //exports crea un objeto con el atributo divide, que tiene como valor la funcion divide
-
-console.log(exports)
