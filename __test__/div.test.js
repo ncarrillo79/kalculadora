30f31c7e19ababb259a626b926ae168a7208746a
ncarrillo79 <ncarrillo79@hotmail.com>
2022-03-02 22:22:27 -0300

    agregado teste divisão por zero

--- __test__/div.test.js
+++ __test__/div.test.js
@@ -1,13 +1,19 @@
 const  {divide}  = require('../src/div.js')
 
 
-describe('test para suma', ()=>{
+describe('test para divide', ()=>{
     test('15/5 is 3', ()=>{
         expect(divide(15,5)).toBe(3)
     })
     it('should return 3 whit 15 / 5', ()=>{
         expect(divide(15,5)).toBe(3)
     })
+
+    it('should return false whit 15 / 0', ()=>{
+        expect(divide(15,0)).toBe(false)
+    })
+
+    
 
 
 })
